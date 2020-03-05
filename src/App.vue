a45820ef73a1201b6ba2dcc6c03222b8cbb56dbd
YashChatim <chatim.yash@yahoo.co.uk>
2020-03-05 17:24:49 +0000

    Dynamically logs champions name when clicked on selected champion

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="all-champion-container">
+    <Champion name="AATROX" description="THE DARKIN BLADE" />
+    <Champion name="AHRI" description="THE NINE-TAILED FOX" />
+    <Champion name="AKALI" description="THE ROGUE ASSASSIN" />
+    <Champion name="ALISTAR" description="THE MINOTAUR" />
+    <Champion name="AMUMU" description="THE SAD MUMMY" />
+    <Champion name="ANIVIA" description="THE CRYOPHOENIX" />
+    <Champion name="ANNIE" description="THE DARK CHILD" />
+    <Champion name="APHELIOS" description="THE WEAPON OF THE FAITHFUL" />
+    <Champion name="ASHE" description="THE FROST ARCHER" />
+    <Champion name="AURELION SOL" description="THE STAR FORGER" />
+    <Champion name="AZIR" description="THE EMPEROR OF THE SANDS" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Champion from './components/Champion.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Champion
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background-color: lightgray;
+}
+
+.all-champion-container {
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Champion.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="champion-container" @click="displayAbilities()">
+        <h1>{{ name }}</h1>
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'champion',
+    props: {
+        name: String,
+        description: String
+    },
+    methods: {
+        displayAbilities() {
+            console.log(`${this.name}`);
+        }
+    }
+}
+</script>
+
+<style>
+    .champion-container {
+        background-color: gray;
+    }
+</style>
